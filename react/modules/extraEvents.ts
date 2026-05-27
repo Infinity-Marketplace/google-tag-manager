2f26cfffa5b34e40f0dd5c8f4d2ec63b26fb01e8
Claudiu Ionita <73105476+claudiu-iviteb@users.noreply.github.com>
2026-05-27 17:07:30 +0300

    add sign_up event

--- react/modules/extraEvents.ts
+++ react/modules/extraEvents.ts
@@ -1,17 +1,76 @@
 import push from './push'
 import { PixelMessage } from '../typings/events'
+import {
+  fetchWithRetry,
+  getCustomProfileFieldValueAsMs,
+  getIsRegister,
+} from './utils'
 
+async function emailToHash(email: string) {
+  const msgUint8 = new TextEncoder().encode(email)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
 
-async function emailToHash(email:string) {
-  const msgUint8 = new TextEncoder().encode(email);                           
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
+  return hashHex
+}
+
+async function checkRegisterEvent(userId: string) {
+  // We first check if we already checked for the register event, to avoid unnecessary calls to the API
+  const hasCheckedRegisterEvent = localStorage.getItem(
+    `hasCheckedRegisterEvent_${userId}`
+  )
+
+  if (hasCheckedRegisterEvent) {
+    return
+  }
+
+  // We fetch createdIn and updatedIn from the profile via vtex.store-graphql
+  const result: {
+    data: { profile: { customFields: Array<{ key: string; value: string }> } }
+  } = await fetchWithRetry('/_v/private/graphql/v1', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+      query Profile {
+        profile(customFields: "createdIn,updatedIn") @context(provider: "vtex.store-graphql@2.x") {
+          customFields {
+              key
+              value
+          }
+        }
+      }
+    `,
+    }),
+  })
+
+  const customFields = result?.data?.profile?.customFields
+
+  if (!customFields?.length) {
+    return
+  }
+
+  const createdIn = getCustomProfileFieldValueAsMs(customFields, 'createdIn')
+
+  const updatedIn = getCustomProfileFieldValueAsMs(customFields, 'updatedIn')
+
+  const isRegisterEvent = getIsRegister(createdIn, updatedIn)
+
+  if (isRegisterEvent) {
+    push({
+      event: 'sign_up',
+      userId,
+    })
+  }
+
+  // We set a flag in localStorage to avoid checking for the register event again
+  localStorage.setItem(`hasCheckedRegisterEvent_${userId}`, 'true')
 }
 
 export async function sendExtraEvents(e: PixelMessage) {
-
   switch (e.data.eventName) {
     case 'vtex:pageView': {
       push({
@@ -34,12 +93,14 @@ export async function sendExtraEvents(e: PixelMessage) {
         return
       }
 
+      checkRegisterEvent(data.id as string)
+
       const emailHash = data.email ? await emailToHash(data.email) : undefined
 
       push({
         event: 'userData',
         userId: data.id,
-        emailHash: emailHash 
+        emailHash,
       })
 
       break
